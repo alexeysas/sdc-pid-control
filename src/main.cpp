511ad5eb92022e23636413d7c14a3963b637b4c0
Alexey Sas <asasryl@gmail.com>
2018-01-04 22:19:43 +0300

    some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -83,7 +83,7 @@ int main()
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
       } else {
